9782c09471cc68a6944e0c25258d166efd3b5602
mikaelo.duh <mikaelo.duh@gmail.com>
2019-12-22 09:05:26 +0800

    fix: pass all test cases

--- main.py
+++ main.py
@@ -1,35 +1,34 @@
-import datetime
 from calendar import monthrange
+from datetime import datetime
 
 
 class Accounting:
+
     def query_budget(self, start, end):
-        # '202001' '202002' '202003'
         data = IBudgetRepo().get_all()
 
         str_start = start.strftime('%Y%m')
         str_end = end.strftime('%Y%m')
 
         # 相差幾天
-        number_of_date_start = end.day - start.day + 1
-        number_of_date_end = end.day - start.day + 1
-        print(number_of_date)
-        #
-        number_of_date_in_the_start_month = monthrange(start.year, start.month)[1]
-        number_of_date_in_the_end_month = monthrange(end.year, end.month)[1]
+        total_number_of_date_in_the_start_month = monthrange(start.year, start.month)[1]
+        total_number_of_date_in_the_end_month = monthrange(end.year, end.month)[1]
+
+        number_of_date_in_the_start_month = total_number_of_date_in_the_start_month - start.day + 1
+        number_of_date_in_the_end_month = end.day
 
         budget = 0
         for k, v in data.items():
-            if k == str_start:
-                budget += v.amount *
-            if k == str_end:
+            if k == str_start and (start.year == end.year) and (start.month == end.month):
+                budget = budget + v.amount * (end.day - start.day + 1) // total_number_of_date_in_the_start_month
+            elif k == str_start:
+                budget = budget + (v.amount * number_of_date_in_the_start_month // total_number_of_date_in_the_start_month)
+            elif str_start < k < str_end:
+                budget = budget + v.amount
+            elif k == str_end:
+                budget = budget + (v.amount * number_of_date_in_the_end_month // total_number_of_date_in_the_end_month)
 
-            # n[str_start].year_month
-            # n[str_end].year_month
-        return budget * number_of_date // (number_of_date_in_the_start_month +
-                                           number_of_date_in_the_end_month)
-        # budget = data.get(start)
-        # return budget
+        return budget
 
 
 class IBudgetRepo:
@@ -44,15 +43,8 @@ class IBudgetRepo:
         self.db[str_date] = budget
 
 
-class BudgetModel():
-    year_month = ''
-    amount = 0
+class BudgetModel:
 
     def __init__(self, year_month, amount):
         self.year_month = year_month
         self.amount = amount
-
-
-# if __name__ == "__main__":
-#     budget = Accounting().query_budget(1, 2)
-#     print(budget)
--- test_account_budget.py
+++ test_account_budget.py
@@ -1,23 +1,29 @@
 from main import Accounting, IBudgetRepo
 import datetime
+import pytest
 
 
-class TestAccountingBudget():
-    def test_no_budget(self):
+class TestAccountingBudget:
+
+    @pytest.fixture
+    def accounting(self):
+        return Accounting()
+
+    def test_no_budget(self, accounting):
         self.create_budget_data(datetime.datetime(2020, 1, 1), 0)
-        budget = Accounting().query_budget(datetime.datetime(2020, 1, 1),
-                                           datetime.datetime(2020, 3, 30))
+        budget = accounting.query_budget(datetime.datetime(2020, 1, 1),
+                                         datetime.datetime(2020, 3, 30))
 
         assert budget == 0
 
-    def test_one_month_budget(self):
+    def test_one_whole_month_budget(self):
         self.create_budget_data(datetime.datetime(2020, 1, 1), 310)
         budget = Accounting().query_budget(datetime.datetime(2020, 1, 1),
                                            datetime.datetime(2020, 1, 31))
 
         assert budget == 310
 
-    def test_two_month_budget(self):
+    def test_two_whole_month_budget(self):
         self.create_budget_data(datetime.datetime(2019, 11, 1), 300)
         self.create_budget_data(datetime.datetime(2019, 12, 31), 310)
         budget = Accounting().query_budget(datetime.datetime(2019, 11, 1),
@@ -32,11 +38,14 @@ class TestAccountingBudget():
 
         assert budget == 100
 
-    # def test_get_all_budget(self):
-    #     self.create_budget_data('201911', 300)
-    #     self.create_budget_data('201912', 310)
-    # def test_one_mouth():
-    #     pass
+    def test_all_budget(self):
+        self.create_budget_data(datetime.datetime(2078, 11, 1), 300)
+        self.create_budget_data(datetime.datetime(2078, 12, 1), 200)
+
+        budget = Accounting().query_budget(datetime.datetime(2078, 1, 1),
+                                           datetime.datetime(2087, 1, 1))
+
+        assert budget == 500
 
     @staticmethod
     def create_budget_data(date, amount):
