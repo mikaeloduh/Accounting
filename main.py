390c03e274480a36508ad32e241e8260319042a1
mikaelo.duh <mikaelo.duh@gmail.com>
2019-12-23 16:57:32 +0800

    use unittest class instead of builtin assert

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from datetime import datetime
 
 class Accounting:
 
-    def query_budget(self, start, end):
-        data = IBudgetRepo().get_all()
+    def __init__(self, budget_repo):
+        self.repo = budget_repo
 
+    def query_budget(self, start, end):
         str_start = start.strftime('%Y%m')
         str_end = end.strftime('%Y%m')
 
@@ -18,11 +19,12 @@ class Accounting:
         number_of_date_in_the_end_month = end.day
 
         budget = 0
-        for k, v in data.items():
+        for k, v in self.repo.get_all().items():
             if k == str_start and (start.year == end.year) and (start.month == end.month):
                 budget = budget + v.amount * (end.day - start.day + 1) // total_number_of_date_in_the_start_month
             elif k == str_start:
-                budget = budget + (v.amount * number_of_date_in_the_start_month // total_number_of_date_in_the_start_month)
+                budget = budget + (
+                            v.amount * number_of_date_in_the_start_month // total_number_of_date_in_the_start_month)
             elif str_start < k < str_end:
                 budget = budget + v.amount
             elif k == str_end:
--- test_account_budget.py
+++ test_account_budget.py
@@ -1,52 +1,56 @@
 from main import Accounting, IBudgetRepo
+
+import unittest
 import datetime
-import pytest
 
 
-class TestAccountingBudget:
+class TestAccountingBudget(unittest.TestCase):
 
-    @pytest.fixture
-    def accounting(self):
-        return Accounting()
+    def setUp(self):
+        self.repo = IBudgetRepo()
+        self.accounting = Accounting(self.repo)
 
-    def test_no_budget(self, accounting):
+    def test_no_budget(self):
         self.create_budget_data(datetime.datetime(2020, 1, 1), 0)
-        budget = accounting.query_budget(datetime.datetime(2020, 1, 1),
-                                         datetime.datetime(2020, 3, 30))
+        budget = self.accounting.query_budget(datetime.datetime(2020, 1, 1),
+                                              datetime.datetime(2020, 3, 30))
 
-        assert budget == 0
+        self.assertEqual(budget, 0)
 
     def test_one_whole_month_budget(self):
         self.create_budget_data(datetime.datetime(2020, 1, 1), 310)
-        budget = Accounting().query_budget(datetime.datetime(2020, 1, 1),
-                                           datetime.datetime(2020, 1, 31))
+        budget = self.accounting.query_budget(datetime.datetime(2020, 1, 1),
+                                              datetime.datetime(2020, 1, 31))
 
-        assert budget == 310
+        self.assertEqual(budget, 310)
 
     def test_two_whole_month_budget(self):
         self.create_budget_data(datetime.datetime(2019, 11, 1), 300)
         self.create_budget_data(datetime.datetime(2019, 12, 31), 310)
-        budget = Accounting().query_budget(datetime.datetime(2019, 11, 1),
-                                           datetime.datetime(2019, 12, 31))
+        budget = self.accounting.query_budget(datetime.datetime(2019, 11, 1),
+                                              datetime.datetime(2019, 12, 31))
 
-        assert budget == 610
+        self.assertEqual(budget, 610)
 
     def test_one_third_month_budget(self):
         self.create_budget_data(datetime.datetime(2019, 4, 1), 300)
-        budget = Accounting().query_budget(datetime.datetime(2019, 4, 1),
-                                           datetime.datetime(2019, 4, 10))
+        budget = self.accounting.query_budget(datetime.datetime(2019, 4, 1),
+                                              datetime.datetime(2019, 4, 10))
 
-        assert budget == 100
+        self.assertEqual(budget, 100)
 
     def test_all_budget(self):
         self.create_budget_data(datetime.datetime(2078, 11, 1), 300)
         self.create_budget_data(datetime.datetime(2078, 12, 1), 200)
 
-        budget = Accounting().query_budget(datetime.datetime(2078, 1, 1),
-                                           datetime.datetime(2087, 1, 1))
+        budget = self.accounting.query_budget(datetime.datetime(1970, 1, 1),
+                                              datetime.datetime(2087, 1, 1))
 
-        assert budget == 500
+        self.assertEqual(budget, 500)
 
-    @staticmethod
-    def create_budget_data(date, amount):
-        IBudgetRepo().insert_data(date, amount)
+    def create_budget_data(self, date, amount):
+        self.repo.insert_data(date, amount)
+
+
+if __name__ == '__main__':
+    unittest.main()
