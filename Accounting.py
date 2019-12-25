0e160ced96fe1bdee22a720728f8974cfc6e687a
mikaelo.duh <mikaelo.duh@gmail.com>
2019-12-25 23:54:20 +0800

    remove unnecessary calculation

--- Accounting.py
+++ Accounting.py
@@ -16,19 +16,17 @@ class Accounting:
         total_date_end_month = monthrange(end.year, end.month)[1]
 
         date_start_month = total_date_start_month - start.day + 1
-        date_end_month = end.day
-
         total_budget = 0
         for k, v in self.repo.get_all().items():
             if k == str_start and (start.year == end.year) and (start.month == end.month):
                 total_budget = v.amount * (end.day - start.day + 1) // total_date_start_month
                 break
             elif k == str_start:
-                total_budget = total_budget + (v.amount * date_start_month // total_date_start_month)
+                total_budget = v.amount * date_start_month // total_date_start_month
             elif str_start < k < str_end:
                 total_budget = total_budget + v.amount
             elif k == str_end:
-                total_budget = total_budget + (v.amount * date_end_month // total_date_end_month)
+                total_budget = total_budget + (v.amount * end.day // total_date_end_month)
                 break
 
         return total_budget
